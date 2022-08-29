0313570faa27170bb6bb4ca85e543ce628e20879
nazar15yurchuk <59067488+nazar15yurchuk@users.noreply.github.com>
2022-08-29 18:01:50 +0300

    Lab_01

---
+++ Lab_01/src/Lab_01.java
@@ -0,0 +1,9 @@
+public class Lab_01
+{
+		public static void main(String[] args)
+		{
+			String message = new String();
+			message = "Yurchuk Nazar, KI-34";
+			System.out.println(message);
+		}
+}
